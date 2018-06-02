3c32272b512903d52ae68bfaf41e5ffc056a88bd
shariharan205 <shariharan205@gmail.com>
2018-06-01 22:32:40 -0700

    Utils file

---
+++ Utils.py
@@ -0,0 +1,16 @@
+import calendar
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import chain, combinations
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#Read the data
+data = pd.read_csv("network_backup_dataset.csv")
