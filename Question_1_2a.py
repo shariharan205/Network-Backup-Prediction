2123422c059ae5c0e21186c65a3c251ade251e26
shariharan205 <shariharan205@gmail.com>
2018-06-27 23:09:14 -0700

    Updated utils

--- Question_1_2a.py
+++ Question_1_2a.py
@@ -1,7 +1,3 @@
-"""
-Utils file for Network Backup Prediction 
-Consists methods that encodes the data, performs regression, regularization and optimization
-"""
 import calendar
 import pandas as pd
 import numpy as np
@@ -105,7 +101,6 @@ def linear_regression(X, y, plot=True, model=LinearRegression()):
 def f_regr(X, Y):
    return f_regression(X,Y,center = False)
 
-
 def mutual_info_regr(X, Y):
     return mutual_info_regression(X, Y)
 
@@ -122,11 +117,12 @@ def scalar_encode(data, input_features, output_col_name):
     return X, y
 
 
-"""===============================Plotting backup sizes for all workflows for a 20day and 105day period====================="""
+"""===================================================Question 1====================================================="""
 
 data['day_number'] = (data['Week #']-1)*7+ scalar_encode_day(data['Day of Week'])
 data['Workflow_ID'] = scalar_encode_underscored(data['Work-Flow-ID'])
 
+#Question 1a)
 data_20 = data[data['day_number']<=20]
 
 plt.figure(figsize=(8,5))
@@ -135,6 +131,7 @@ plt.xlabel('Day number')
 plt.ylabel('Backup size (GB)')
 plt.show()
 
+#Question 1b)
 plt.figure(figsize=(8,5))
 plt.scatter(x=data['day_number'],y=data['Size of Backup (GB)'],c=data['Workflow_ID'], s = 8)
 plt.xlabel('Day number')
@@ -142,7 +139,7 @@ plt.ylabel('Backup size (GB)')
 plt.show()
 
 
-"""========================================Data Preprocessing and Linear Regression============================================="""
+"""===================================================Question 2a (i)================================================"""
 
 print("Fit linear regression model after scalar encoding")
 input_features = ['Week #', 'Day of Week', 'Backup Start Time - Hour of Day', 'Work-Flow-ID', 'File Name']
@@ -150,7 +147,15 @@ output_col_name = ['Size of Backup (GB)']
 X, y = scalar_encode(data, input_features, output_col_name)
 linear_regression(X, y)
 
-"""===================================Feature Selection using f_regression and mutual information regression=============================="""
+"""===================================================Question 2a (ii)==============================================="""
+
+print("Fit linear regression model after data preprocessing - Standardizing")
+scaler = StandardScaler()
+std_input = np.copy(X)
+scaler.fit(std_input)
+linear_regression(scaler.transform(std_input), y)
+
+"""==================================================Question 2a (iii)==============================================="""
 
 print("Linear Regression Model after Feature Selection")
 most_imp_var_num = 3
@@ -163,7 +168,8 @@ print("Mutual Information Regression")
 X_mutualinfo_regr = SelectKBest(score_func = mutual_info_regr, k = most_imp_var_num).fit_transform(X, y)
 linear_regression(X_mutualinfo_regr, y)
 
-"""==============================Feature Encoding - Analyzing all possible combinations of the categorical variables============================="""
+
+"""===================================================Question 2a(iv)================================================"""
 
 feature_range = range(5)
 feature_powerset = list(powerset(feature_range))
@@ -221,4 +227,72 @@ print("Minimum Train RMSE " , min(avg_train_rmse), " for combination " , np.argm
 print("Minimum Test RMSE " ,  min(avg_test_rmse), " for combination " ,  np.argmin(avg_test_rmse))
 
 
+"""===================================================Question 2a(v)================================================="""
 
+alpha_values = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5,10]
+
+def regularization_optimization(model, alpha_range=alpha_values):
+    alpha_rmse = []
+
+    for alpha in alpha_range:
+
+        # print('Testing for Regularization strength = ', alpha)
+        avg_test_rmse = []
+        for combination in feature_powerset:
+            X_copy = np.copy(X)
+            enc = OneHotEncoder(categorical_features=list(combination))
+            onehot_encoded = enc.fit_transform(X_copy)
+
+            if type(onehot_encoded) != np.ndarray:
+                onehot_encoded = onehot_encoded.toarray()
+
+            _, test_rmse = linear_regression(onehot_encoded, y, plot=False, model=model(alpha=alpha))
+            avg_test_rmse.append(test_rmse)
+
+        alpha_rmse.append(avg_test_rmse)
+
+    return alpha_rmse
+
+
+regularizations = ["Ridge", "Lasso", "ElasticNet"]
+
+
+for reg in regularizations:
+
+    print("Finding the best parameters for ", reg)
+    plt.figure(figsize=(8, 5))
+    plt.xlabel("One-hot encoded Model")
+    plt.ylabel("Test RMSE")
+    plt.title("Test RMSE for One-hot encoded models for " + reg)
+
+    # alpha_range = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5,10]
+    alpha_range = alpha_values
+    alpha_rmse = regularization_optimization(model=eval(reg), alpha_range=alpha_range)
+
+    for alpha in range(len(alpha_range)):
+        print(alpha, min(alpha_rmse[alpha]), np.argmin(alpha_rmse[alpha]))
+        plt.plot(x_range, alpha_rmse[alpha], label=alpha_range[alpha])
+
+    plt.legend(title="Regularization strength")
+    plt.show()
+
+
+
+X_copy = np.copy(X)
+enc = OneHotEncoder(categorical_features = [1, 2, 3, 4])
+onehot_encoded = enc.fit_transform(X_copy)
+
+if type(onehot_encoded) != np.ndarray:
+    onehot_encoded = onehot_encoded.toarray()
+
+train_rmse, test_rmse = linear_regression(onehot_encoded, y, model = Lasso(alpha = 0.001))
+print(train_rmse,test_rmse)
+
+train_rmse, test_rmse = linear_regression(onehot_encoded, y, model = ElasticNet(alpha = 0.001))
+print(train_rmse,test_rmse)
+
+train_rmse, test_rmse = linear_regression(onehot_encoded, y, model = Ridge(alpha = 0.001))
+print(train_rmse,test_rmse)
+
+
+"""=================================================================================================================="""
