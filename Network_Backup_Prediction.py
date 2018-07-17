e912ce01063d57daa6117d1594d25f1e5770c054
shariharan205 <shariharan205@gmail.com>
2018-07-16 23:42:07 -0700

    Updated KFold Cross validation

--- Network_Backup_Prediction.py
+++ Network_Backup_Prediction.py
@@ -46,3 +46,39 @@ scalar_data = data[['Week #','day','Backup Start Time - Hour of Day','Workflow_I
 scalar_data_X = scalar_data[['Week #','day','Backup Start Time - Hour of Day',
 'Workflow_ID','File_number']]
 scalar_data_y = scalar_data[['Size of Backup (GB)']]                     
+                      
+#K Fold Cross validation as a function
+def kfold_cv(X,y,return_errors=False,regression='Linear',features=5,trees=20,depth=4,k=5,
+             return_model=False):
+    train_error_k, test_error_k = [], []
+    kf = KFold(n_splits=5,shuffle=False)
+    best_mse=1e8
+    for trainset, testset in kf.split(X):
+        if regression == 'KNN':
+            #print('Using KNN Regression')
+            regressor = KNeighborsRegressor(n_neighbors=k)
+        elif regression == 'RF':
+            #print('Using random forest regression')
+            regressor = RandomForestRegressor(n_estimators=trees,max_depth=depth,
+                                              max_features=features,
+                                              bootstrap=True)
+        else:
+            regressor = LinearRegression()
+        regressor.fit(X=X.iloc[trainset],y=y.iloc[trainset].values.ravel())
+        y_pred_train = regressor.predict(X.iloc[trainset])
+        train_mse = mean_squared_error(y.iloc[trainset],y_pred_train) 
+        train_error_k.append(train_mse)
+        y_pred_test = regressor.predict(X.iloc[testset])
+        test_mse = mean_squared_error(y.iloc[testset],y_pred_test)
+        test_error_k.append(test_mse)
+        if test_mse < best_mse:
+            best_model = regressor
+    training_error = np.sqrt(np.mean(train_error_k))
+    test_error = np.sqrt(np.mean(test_error_k))    
+    print('The training error is', training_error)    
+    print('The testing error is', test_error)    
+    if return_errors:
+        return training_error,test_error
+    if return_model:
+        return best_model
+    
